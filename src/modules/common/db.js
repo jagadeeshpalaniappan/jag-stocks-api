145238c13e294125f472a5da09275a15f03ae889
Jagadeesh <jagadeeshthegeek@gmail.com>
2020-12-14 12:18:35 +0530

    cu

--- src/modules/common/db.js
+++ src/modules/common/db.js
@@ -11,10 +11,13 @@ let isDbInitialized = null; // Create cached connection variable
 async function initDBConnection() {
   try {
     if (isDbInitialized) return;
+
+    console.log("initDBConnection:start", { DB_URL });
     await mongoose.connect(DB_URL, { useNewUrlParser: true });
+    console.log("initDBConnection:end", { DB_URL });
   } catch (error) {
     const err = { errCode: "DB-INIT-ERR", error };
-    console.error(err);
+    console.error("initDBConnection:err", err);
     throw err;
   }
 }
