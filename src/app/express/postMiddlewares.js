c2731a1bc6aeb7f3dd6fefcfa35e65fa6d830266
Jagadeesh <jagadeeshthegeek@gmail.com>
2021-01-18 02:30:25 +0530

    cu

--- src/app/express/postMiddlewares.js
+++ src/app/express/postMiddlewares.js
@@ -22,20 +22,39 @@ const convertErrToAPIError = (err) => {
   return err;
 };
 
-module.exports = (app) => {
-  // if error is not an instanceOf APIError, convert it.
-  app.use((err, req, res, next) => {
+function handleErr() {
+  return (err, req, res, next) => {
     console.error("ERR1", err);
     const apiError = convertErrToAPIError(err);
     return next(apiError);
-  });
+  };
+}
 
-  // catch 404 and forward to error handler
-  app.use((req, res, next) => {
+function handle404() {
+  return (req, res, next) => {
     // console.error("ERR2: 404");
     const err = new APIError("API not found", httpStatus.NOT_FOUND);
     return next(err);
-  });
+  };
+}
+
+// error handler, send stacktrace only during development
+function handleErrToResp() {
+  // eslint-disable-next-line no-unused-vars
+  return (err, req, res, next) => {
+    return res.status(err.status).json({
+      message: err.isPublic ? err.message : httpStatus[err.status],
+      stack: config.env === "development" ? err.stack : {},
+    });
+  };
+}
+
+module.exports = (app) => {
+  // if error is not an instanceOf APIError, convert it.
+  app.use(handleErr());
+
+  // catch 404 and forward to error handler
+  app.use(handle404());
 
   // log error in winston transports except when executing test suite
   // if (config.env !== "test") {
@@ -46,12 +65,5 @@ module.exports = (app) => {
   //   );
   // }
 
-  // error handler, send stacktrace only during development
-  // eslint-disable-next-line no-unused-vars
-  app.use((err, req, res, next) => {
-    return res.status(err.status).json({
-      message: err.isPublic ? err.message : httpStatus[err.status],
-      stack: config.env === "development" ? err.stack : {},
-    });
-  });
+  app.use(handleErrToResp());
 };
